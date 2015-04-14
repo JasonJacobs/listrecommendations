3708cdca25fd397d06369a4c5404578624b68744
casie <casie.levine@gmail.com>
2015-04-14 16:17:48 -0400

    started ajax-ifying the lists view

--- app/assets/javascripts/lists.coffee
+++
@@ -1,4 +0,0 @@
-$(document).ready ->
-	$('#add_recommendation_form').on "click", ->
-		html = $.get "/recommendations/new.js"
-		$('#new_recommendations').append(html)
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   end
 
   root to: "static_pages#welcome"
+  get "add_recommendation_form" => "lists#add_recommendation_form"
+
 end
